fb74f06a89e6d27b94dca8401ecf9e7f68fb4067
SergioEspinar <sec0008@alu.medac.es>
2025-11-03 13:15:29 +0100

    Mi segundo commit

--- src/main/java/com/mycompany/proyecto_supermercado/Proyecto_supermercado.java
+++ src/main/java/com/mycompany/proyecto_supermercado/Proyecto_supermercado.java
@@ -11,6 +11,6 @@ package com.mycompany.proyecto_supermercado;
 public class Proyecto_supermercado {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! por Sergio");
     }
 }
