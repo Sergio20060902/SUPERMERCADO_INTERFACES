3ac344e2df2219e5750e46b3a84bf064884f0b13
SergioEspinar <sec0008@alu.medac.es>
2025-11-03 12:54:24 +0100

    Mi primer commit

---
+++ src/main/java/com/mycompany/proyecto_supermercado/Proyecto_supermercado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_supermercado;
+
+/**
+ *
+ * @author MEDAC
+ */
+public class Proyecto_supermercado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
